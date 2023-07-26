22cd8134be8e402011046ffef9338972b2024c80
MASTERALD <alexmeraz4567@gmail.com>
2023-07-26 17:27:47 -0600

    Agregar archivos de proyecto.

---
+++ PantallaPagos/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using PantallaPagos.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PantallaPagos
+{
+    public partial class MainPage : ContentPage
+    {
+        public  Pagos pago;
+        public MainPage()
+        {
+            InitializeComponent();
+            pago = new Pagos();
+        }
+
+        private void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            // Aquí se puede navegar hacia atrás a la página anterior
+            Navigation.PopAsync();
+        }
+
+        private void OnPagarButtonClicked(object sender, EventArgs e)
+        {
+            // Verificar si se han ingresado todos los datos requeridos y si se ha seleccionado un método de pago
+            if (!string.IsNullOrEmpty(NombresEntry.Text) &&
+                !string.IsNullOrEmpty(CorreoEntry.Text) &&
+                !string.IsNullOrEmpty(TelefonoEntry.Text) &&
+                !string.IsNullOrEmpty(CedulaEntry.Text) &&
+                (TarjetaRadio.IsChecked || GiftCardRadio.IsChecked) &&
+                TerminosCheckbox.IsChecked)
+            {
+                // Asignar los valores ingresados a la instancia de Pagos
+                pago.Nombres = NombresEntry.Text;
+                pago.CorreoElectronico = CorreoEntry.Text;
+                pago.NumeroTelefonico = TelefonoEntry.Text;
+                pago.CedulaIdentidad = CedulaEntry.Text;
+
+                // Aquí se puede realizar el proceso de pago y mostrar una confirmación o mensaje de éxito
+                // Por ejemplo, mostrar un mensaje emergente con los datos de pago
+                string metodoPago = TarjetaRadio.IsChecked ? "Tarjeta Crédito/Debito" : "GiftCard";
+                string mensaje = $"Pago realizado:\nNombres: {pago.Nombres}\nCorreo: {pago.CorreoElectronico}\nTeléfono: {pago.NumeroTelefonico}\nCédula: {pago.CedulaIdentidad}\nMétodo de Pago: {metodoPago}";
+                DisplayAlert("Pago Exitoso", mensaje, "Aceptar");
+            }
+            else
+            {
+                // Si faltan datos o no se han aceptado los términos, mostrar un mensaje de error
+                DisplayAlert("Error", "Por favor, complete todos los campos y acepte los términos para continuar.", "Aceptar");
+            }
+        }
+    }
+}
+
+
---
+++ PantallaPagos/Models/Pagos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PantallaPagos.Models
+{
+    public  class Pagos
+    {
+            public string Nombres { get; set; }
+            public string CorreoElectronico { get; set; }
+            public string NumeroTelefonico { get; set; }
+            public string CedulaIdentidad { get; set; }
+        }
+
+}
+
+
